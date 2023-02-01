8e5e7fcd76e5c46faa05adf7f97f944b5a853d07
fadwa2022 <fadwacherqui@gmail.com>
2023-02-01 20:44:59 +0100

    API

---
+++ api/post/delete.php
@@ -0,0 +1,33 @@
+<?php
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: PUT');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+include_once '../../config/Database.php';
+include_once '../../models/Post.php';
+
+// Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate blog post object
+$post = new Post($db);
+
+// Get raw posted data
+$data = json_decode(file_get_contents("php://input"));
+//   Set ID to  delete
+$post->id = $data->id;
+
+
+// delete post
+if ($post->delete()) {
+    echo json_encode(
+        array('message' => 'Post Deleted')
+    );
+} else {
+    echo json_encode(
+        array('message' => 'Post Not Deleted')
+    );
+}
