821d23989e1e670ea400c7dc04bf0cc787d1a532
fadwa2022 <fadwacherqui@gmail.com>
2023-02-07 15:28:40 +0100

    partie backend

--- backend/api/movies/create.php
+++ backend/api/movies/create.php
@@ -18,6 +18,7 @@
   // Get raw posted data
   $data = json_decode(file_get_contents("php://input"));
 
+  
   $user->title = $data->title;
   $user->image = $data->image;
   $user->description = $data->description;
